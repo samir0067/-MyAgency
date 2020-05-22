3ea2920f87925c3cc0e3adb41d9fde2a579fa243
samir0067 <arrahmani.samir@gmail.com>
2020-05-22 20:05:42 +0200

    ajout de plusieur lien

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -41,6 +41,7 @@ class PropertyController extends AbstractController
     /**
      * @Route("/biens/{slug}-{id}", name="property.show", requirements={"slug": "[a-z0-9\-]*"})
      * @param Property $property
+     * @param string $slug
      * @return Response
      */
     public function show(Property $property, string $slug): Response
