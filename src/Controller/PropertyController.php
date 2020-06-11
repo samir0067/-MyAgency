d22e6e9c359fdc82bf6f22c038a987635cefba34
samir0067 <arrahmani.samir@gmail.com>
2020-06-11 15:55:27 +0200

    ajout des filtre de recherche

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 
 use App\Entity\Property;
+use App\Entity\PropertySearch;
+use App\Form\PropertySearchType;
 use App\Repository\PropertyRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Knp\Component\Pager\Pagination\PaginationInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -31,18 +33,25 @@ class PropertyController extends AbstractController
 
     /**
      * @Route("/biens", name="property.index")
-     * @param PaginationInterface $paginator
      * @param Request $request
+     * @param PaginatorInterface $paginator
      * @return Response
      */
-    public function index(PaginationInterface $paginator, Request $request): Response
+    public function index(Request $request, PaginatorInterface $paginator): Response
     {
-        $properties = $paginator->getCurrentPageNumber();
+        $search = new PropertySearch();
+        $form = $this->createForm(PropertySearchType::class, $search);
+        $form->handleRequest($request);
 
-
+        $properties = $paginator->paginate(
+            $this->repository->findAllVisibleQuery($search),
+            $request->query->getInt('page', 1),
+            8
+        );
         return $this->render('property/index.html.twig', [
             'current_menu' => 'properties',
-            'properties' => $properties
+            'properties' => $properties,
+            'form' => $form->createView(),
         ]);
     }
 
